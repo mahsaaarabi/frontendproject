f29eb9411107756cccd305270aa0cc30fb9429c0
mahsaaarabi <mahsaaarabi950@gmail.com>
2023-08-11 21:50:38 +0330

    v 0.6

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -2,7 +2,8 @@ const swiper = new Swiper('.main-slider', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-
+    slidesPerView: "auto",
+    spaceBetween: 30,
 
     // Navigation arrows
     navigation: {
